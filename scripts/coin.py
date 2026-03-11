23490e6711f1bd3356829853330b9cf00424f2f4
Hagai Lev Hacohen <hagai4000@gmail.com>
2026-03-11 16:30:53 +0200

    added coins

--- scripts/coin.py
+++ scripts/coin.py
@@ -40,4 +40,46 @@ COINS = {
         contract="8453",
         memo=""
     ),
+    "ARB": CryptoCoin( # ARBITUM
+        symbol="ETH",
+        chainId="ARETH",
+        contract="42161",
+        memo=""
+    ),
+    "OPT": CryptoCoin( # OPTIMISM
+        symbol="ETH",
+        chainId="OPTIMISM",
+        contract="10",
+        memo=""
+    ),
+    "TRON": CryptoCoin( # TRON
+        symbol="USDT",
+        chainId="TRX",
+        contract="TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+        memo=""
+    ),
+    "POLY": CryptoCoin( # POLYGON
+        symbol="USDT",
+        chainId="POLYGON",
+        contract="0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+        memo=""
+    ),
+    "SOL": CryptoCoin( # SOLANA
+        symbol="USDT",
+        chainId="SOL",
+        contract="Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
+        memo=""
+    ),
+    "OPBNB": CryptoCoin( # OPBNB
+        symbol="USDT",
+        chainId="OPBNB",
+        contract="0x9e5AAC1Ba1a2e6aEd6b32689DFcF62A509Ca96f3",
+        memo=""
+    ),
+    "AVAX": CryptoCoin( # AVAX
+        symbol="USDT",
+        chainId="AVAX",
+        contract="0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7",
+        memo=""
+    ),
 }
